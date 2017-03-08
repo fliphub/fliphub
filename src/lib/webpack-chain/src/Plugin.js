f8a7a4a27088039729ca9909296ef071b4318491
Arete Code <aretecode@gmail.com>
2017-03-08 07:21:34 -0800

    adjust package script, add webpack-chain and deepmerge

---
+++ src/lib/webpack-chain/src/Plugin.js
@@ -0,0 +1,39 @@
+const merge = require('deepmerge')
+
+module.exports = class {
+  constructor(plugin = () => null, args = []) {
+    this.plugin = plugin
+    this.args = args
+  }
+
+  init(Plugin, args) {
+    if (typeof Plugin === 'function') {
+      return this.args.length ?
+        new Plugin(...args) :
+        new Plugin()
+    }
+
+    return Plugin
+  }
+
+  tap(handler) {
+    this.args = handler(this.args) || this.args
+  }
+
+  inject(handler) {
+    this.init = handler
+    return this
+  }
+
+  merge(obj) {
+    if (obj.plugin) {
+      this.plugin = plugin
+    }
+
+    if (obj.args) {
+      this.args = merge(this.args, obj.args)
+    }
+
+    return this
+  }
+}
